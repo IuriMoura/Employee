cff6959cfdfd9e4e8bd3270f05308ca464fb0c02
IuriMoura <iurirafaelmoura@gmail.com>
2025-01-27 17:37:29 -0300

    Controller

---
+++ src/main/java/com/br/Employee/Controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.br.Employee.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping
+    public ResponseEntity getAllEmployee(){
+        return ResponseEntity.ok("ok");
+    }
+}

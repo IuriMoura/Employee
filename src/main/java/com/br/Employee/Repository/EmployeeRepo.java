e121eae8b056cf67d78115493b6450684689d06a
IuriMoura <iurirafaelmoura@gmail.com>
2025-01-27 17:41:40 -0300

    Repository

--- src/main/java/com/br/Employee/Repository/EmployeeRepo.java
+++ src/main/java/com/br/Employee/Repository/EmployeeRepo.java
@@ -1,6 +1,7 @@
 package com.br.Employee.Repository;
 
+import com.br.Employee.Domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepo extends JpaRepository {
+public interface EmployeeRepo extends JpaRepository<Employee, String> {
 }
